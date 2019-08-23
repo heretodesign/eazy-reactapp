df95b5ed9b30a1d7f9e20876ef7f08a776b081cc
heretodesign <senwelo.works@gmail.com>
2019-08-23 12:19:38 +0800

    added react lazy to split the code in router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react';
 import { Button, table, thead, tbody, columns, column} from "react-bulma-components/full"
 import 'react-bulma-components/dist/react-bulma-components.min.css'
 import Flickr from './Flickr-1.4s-200px.svg';
 import { Route, Link } from "react-router-dom"
 import Nav from './components/layouts/Nav'
-import ListPage from './pages/ListPage.js'
-import ViewPage from './pages/ViewPage.js'
 import axios from 'axios'
 import './App.scss'
+
+const ListPage = lazy(() => import('./pages/ListPage'));
+const ViewPage = lazy(() => import('./pages/ViewPage'));
 
 function App() {
   return (
     <section className="App">
       <Nav />
-      <Route exact path="/" component={ListPage} />
-      <Route path="/pages/viewpage/:id" component={ViewPage} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route exact path="/" component={ListPage} />
+        <Route path="/pages/viewpage/:id" component={ViewPage} />
+      </Suspense>
     </section>
   );
 }
